61cff1803802ccd301b817164243eb0b3c313af4
Devanee Reid <devaneereid@gmail.com>
2019-08-21 16:34:02 -0600

    progress on character stats

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,16 +15,16 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
+
 function GameObject(attributes) {
   this.createdAt = attributes.createdAt,
   this.name = attributes.name,
   this.dimensions = attributes.dimensions,
 }
 
-GameObject.prototype.destroy = function(){
+GameObject.prototype.destroy = function() {
   return `${this.name} was removed from the game.`;
 };
-console.log(GameObject.prototype.destroy());
 
 /*
   === CharacterStats ===
@@ -32,12 +32,18 @@ console.log(GameObject.prototype.destroy());
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
-function CharacterStats(properties){
-  GameObject.call(this, properties);
-  this.healthPoints = properties.healthPoints;
-  this.name = properties.name;
+
+function CharacterStats (attributes){
+  this.healthPoints = attributes.healthPoints;
+  this.name = attributes.name;
+  Object.create(GameObject.prototype);
+  GameObject.call(this, attributes);
 }
-CharacterStats.prototype = 
+Object.create (GameObject.prototype){
+  CharacterStats.prototype.takeDamage = function(){
+  return `${this.name} took damage.`
+}
+console.log(CharacterStats.attributes.call());
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
   * team
